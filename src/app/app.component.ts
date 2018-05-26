6ef2e1a2822c5103ae2d57a08dfec8aecb1eaceb
Rahul Kumar <rahulkumar@rediffmail.com>
2018-05-27 01:45:27 +0530

    UI Changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <ul>
-      <li><a routerLink="/">Home</a></li>
-      <li><a routerLink="/resetpasswordbytoken">ResetPassword</a></li>
-    </ul>
-    <hr />
     <router-outlet></router-outlet>
   `,
 })
